6dea70cdee43e480ff36ba4148d94f2031e29edf
Haruka Kotani <harukakotani28@gmail.com>
2023-05-10 20:59:08 -0700

    added recipes class

---
+++ RecipeAPI/Models/Recipes.cs
@@ -0,0 +1,13 @@
+﻿namespace RecipeAPI.Models
+{
+    public class Recipes
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public string Description { get; set; }
+        public int Servings { get; set; }
+        public int PrepTime { get; set; }
+        public int CookTime { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
