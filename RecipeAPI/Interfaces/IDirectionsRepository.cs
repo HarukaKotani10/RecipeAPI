a0beeeaa3030273d471981064a8e1ebeb74dea25
Haruka Kotani <harukakotani28@gmail.com>
2023-05-30 21:24:31 -0700

    Created direction interface

---
+++ RecipeAPI/Interfaces/IDirectionsRepository.cs
@@ -0,0 +1,15 @@
+﻿using RecipeAPI.Models;
+
+namespace RecipeAPI.Interfaces
+{
+    public interface IDirectionsRepository
+    {
+        ICollection<Directions> GetDirections();
+        Directions GetDirections(int id);
+        bool HasDirections(int id);
+        bool CreateDirection(Directions direction);
+        bool UpdateDirection(Directions direction);
+        bool DeleteDirection(Directions direction);
+        bool Save();
+    }
+}
