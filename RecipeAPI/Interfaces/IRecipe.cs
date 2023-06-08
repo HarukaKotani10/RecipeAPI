447ab5537df24a5f7185cecfcb02fcef92b101cc
Haruka Kotani <harukakotani28@gmail.com>
2023-06-07 21:35:53 -0700

    Added Recipe interface

---
+++ RecipeAPI/Interfaces/IRecipe.cs
@@ -0,0 +1,15 @@
+﻿using RecipeAPI.Models;
+
+namespace RecipeAPI.Interfaces
+{
+    public interface IRecipe
+    {
+        ICollection<Recipes> GetRecipes();
+        Recipes GetRecipe(int id);
+        bool HasRecipe(int id);
+        bool CreateRecipe(Recipes recipe);
+        bool UpdateRecipe(Recipes recipe);
+        bool DeleteRecipe(Recipes recipe);
+        bool Save();
+    }
+}
