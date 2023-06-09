77c2c114ebce1bdac6ef0cb8ca9615a1b974aea3
Haruka Kotani <harukakotani28@gmail.com>
2023-06-08 17:54:29 -0700

    Added recipeingredient interface

---
+++ RecipeAPI/Interfaces/IRecipeIngredientRepository.cs
@@ -0,0 +1,14 @@
+﻿using RecipeAPI.Models;
+
+namespace RecipeAPI.Interfaces
+{
+    public interface IRecipeIngredientRepository
+    {
+        ICollection<RecipeIngredient> GetRecipeIngredients();
+        RecipeIngredient GetRecipeIngredient(int recipeId, int ingredientid);
+        bool CreateRecipeIngredient(RecipeIngredient recipeIngredient);
+        bool UpdateRecipeIngredient(RecipeIngredient recipeIngredient);
+        bool DeleteRecipeIngredient(RecipeIngredient recipeIngredient);
+        bool Save();
+    }
+}
