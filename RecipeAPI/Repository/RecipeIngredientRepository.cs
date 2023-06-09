fb216da27f8cf1e7d9b043552324094bc80ddf8e
Haruka Kotani <harukakotani28@gmail.com>
2023-06-08 18:09:47 -0700

    Added Recipe Ingredient repository

---
+++ RecipeAPI/Repository/RecipeIngredientRepository.cs
@@ -0,0 +1,47 @@
+﻿using RecipeAPI.Data;
+using RecipeAPI.Interfaces;
+using RecipeAPI.Models;
+
+namespace RecipeAPI.Repository
+{
+    public class RecipeIngredientRepository:IRecipeIngredientRepository
+    {
+        private DataContext _context;
+
+        public RecipeIngredientRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool CreateRecipeIngredient(RecipeIngredient recipeIngredient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteRecipeIngredient(RecipeIngredient recipeIngredient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RecipeIngredient GetRecipeIngredient(int recipeId, int ingredientid)
+        {
+            return _context.RecipeIngredients.Where(r => r.RecipeId == recipeId && r.IngredientId == ingredientid).FirstOrDefault();
+        }
+
+        public ICollection<RecipeIngredient> GetRecipeIngredients()
+        {
+            return _context.RecipeIngredients.OrderBy(r => r.RecipeId).ToList();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool UpdateRecipeIngredient(RecipeIngredient recipeIngredient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
